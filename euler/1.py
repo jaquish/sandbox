fd59db6b8faedcb9743f4e3b1070cff0667d10ae
Zachary Jaquish <zach.jaquish@gmail.com>
2012-05-11 17:05:56 -0700

    better understand of pyglet run loop. use clock scheduler to call compute() every n seconds. on_draw is only called by OS triggering redraw (mouse moving over window, change of window focus, etc)

--- euler/1.py
+++ euler/1.py
@@ -1,16 +1,21 @@
 import pyglet
+import time
 
 window = pyglet.window.Window()
 pyglet.gl.glClearColor(.4,.4,.4,1)
 
+BOARD_SIZE = 100
 
 yes, no, unknown = range(3)
+no_color       = (255 ,  20,   20, 255)
+yes_color      = (20  ,  255,  20, 255)
+unknown_color  = (20  ,  20 , 255, 255)
 
 class Board:
 	def __init__(self, size):
 		self.state = [unknown]*size
-
-	def printme(self):
+		
+		self.toDraw = []
 		for i in range(len(self.state)):
 			label = pyglet.text.Label(
 						str(i), 
@@ -18,36 +23,41 @@ class Board:
                         font_size=14,
                         x=(40*i) % (40*10), y= window.height - (i/10 * 40),
                         anchor_x='left', anchor_y='top')
+			self.toDraw.append(label)
 
-			no_color       = (255 ,  20,   20, 255)
-			yes_color      = (20  ,  255,  20, 255)
-			unknown_color  = (20  ,  20 , 255, 255)
+		self.current = 0
 
-			if   (self.state[i] == no):
-				label.color = no_color
-			elif (self.state[i] == yes):
-			 	label.color = yes_color
-			else:
-			 	label.color = unknown_color
-
-			label.draw()
-
-	def compute(self):
+	def printme(self):
+		
 		for i in range(len(self.state)):
-			if (i % 3 == 0):
-				self.state[i] = yes
-			elif (i % 3 == 1):
-				self.state[i] = no
-			else:
-				self.state[i] = unknown	
+			self.toDraw[i].draw()
+
+	def compute_next(self,dt):
+
+		i = self.current
+		if (i >= len(self.state)):
+			return
+
+		if (i % 3 == 0):
+			self.state[i] = yes
+			self.toDraw[i].color = yes_color
+		elif (i % 3 == 1):
+			self.state[i] = no
+			self.toDraw[i].color = no_color
+		else:
+			self.state[i] = unknown	
+			self.toDraw[i].color = unknown_color
+
+		self.current += 1
+		self.toDraw[i].draw()
 
 @window.event
 def on_draw():
 	window.clear()
 	b.printme()
 
-b = Board(100)
+b = Board(BOARD_SIZE)
+
+
+pyglet.clock.schedule_interval(b.compute_next, 0.01)
 pyglet.app.run()
-
-b.compute()
-
